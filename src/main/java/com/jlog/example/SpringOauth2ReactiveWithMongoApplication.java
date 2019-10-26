9dc288a9cd6dd601b9da706ca2477f3144abb93f
juno <mirinae312@gmail.com>
2019-10-26 15:45:31 +0900

    create project

---
+++ src/main/java/com/jlog/example/SpringOauth2ReactiveWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.jlog.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ReactiveWithMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2ReactiveWithMongoApplication.class, args);
+    }
+
+}
