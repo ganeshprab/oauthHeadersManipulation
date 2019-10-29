c7a39f08d22b4e29282cb61ebeeac565f5314c57
juno <mirinae312@gmail.com>
2019-10-29 21:17:03 +0900

    move config from java-code to yml

--- src/main/java/com/jlog/example/_config/MongoSessionConfig.java
+++ src/main/java/com/jlog/example/_config/MongoSessionConfig.java
@@ -3,12 +3,10 @@ package com.jlog.example._config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.mongo.JdkMongoSessionConverter;
-import org.springframework.session.data.mongo.config.annotation.web.reactive.EnableMongoWebSession;
 
 import java.time.Duration;
 
 @Configuration
-@EnableMongoWebSession(collectionName = "sessions")
 public class MongoSessionConfig {
 
     @Bean
